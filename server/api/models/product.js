7bbdc5aab9696cda35981cf5b01f0564332bf8ab
Justas <justas.narijauskas@gmail.com>
2024-11-28 15:19:25 +0100

    updated product schema

--- server/api/models/product.js
+++ server/api/models/product.js
@@ -1,9 +1,14 @@
+
 import mongoose from 'mongoose';
 
 const productSchema = mongoose.Schema({
+
   _id: mongoose.Schema.Types.ObjectId,
+  name: { type: String, required: true },
+  price: { type: Number, required: true },
+  image: { type: String, required: true },
   id: String,
-  toolType: String,   
+  toolType: String,
   description: {
     nameRetail: String,
     basePrice: Number,
@@ -16,5 +21,6 @@ const productSchema = mongoose.Schema({
   reservations: [Object],
   reviews: [Object]
 });
+
 
 export default mongoose.model('Tools', productSchema);
