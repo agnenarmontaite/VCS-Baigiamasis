273f3a9dfd52e5361dac6b3e6f3ed25fa4c21f7d
L1ttl3Cr0w <tomas.brdls@gmail.com>
2024-11-29 15:56:46 +0200

    updated schemas

--- server/api/models/product.js
+++ server/api/models/product.js
@@ -45,6 +45,25 @@ const toolsSchema = new Schema({
       depth_protection: Boolean,
       drilling_with_impact: Boolean,
       drilling_without_impact: Boolean,
+      rotations_per_minuite: Number,
+      max_mill_depth: Number,
+      container_size: Number,
+      dry_vacuum: Boolean,
+      liquid_vacuum: Boolean,
+      vacuum_type: String,
+      chainsaw_type: String,
+      motor_type: String,
+      anti_vibration: Boolean,
+      chain_brakes: String,
+      charger: Boolean,
+      requires_batery: Boolean,
+      chain_step: String,
+      power_supply_type: String,
+      saw_blade_diameter: String,
+      rotation_angle: String,
+      saw_hole_diameter: String,
+      adjustable_support: Boolean,
+      additional_information: String,
       warranty: {type: Number, required: true},
       company_warranty: {type: Number, required: true},
       origin_country: {type: String, required: true}
@@ -58,57 +77,3 @@ const toolsSchema = new Schema({
 }, {timestamps: {createdAt: "createdAt"}});
 
 export default mongoose.model('Tools', toolsSchema);
-
-/*
-const perforatorDetails = new Schema({
-    productType: {type: String, required: true},
-    trademark: {type: String, required: true},
-    model:{type: String, required: true},
-    weight: {type: String, required: true},
-    power: {type: String, required: true},
-    rotatios_per_minute: {type: Number, required: true},
-    impacts_per_minute: {type: String, required: true},
-    impact_energy: {type: Number, required: true},
-    type_of_cartrige: {type: String, required: true},
-    max_drill_diameter_cement: {type: String, required: true},
-    max_drill_diameter_wood: {type: String, required: true},
-    max_drill_diameter_metal: {type: String, required: true},
-    cable_length: {type: Number, required:true},
-    depth_protection: {type: Boolean, required: true},
-    drilling_with_impact: {type: String, required: true},
-    drilling_without_impact: {type: String, required: true},
-    company_warranty: {type: Number, required: true},
-    warranty: {type: Number, required: true},
-    origin_country: {type: String, required: true}
-})
-
-const millDetails = new Schema({
-    productType: {type:String, required: true},
-    trademark: {type:String, required: true},
-    weight: {typr:Number, required: true},
-    power: {type:Number, required: true},
-    rotations_per_minuite: {type:Number, required: true},
-    max_mill_depth: {type:Number, required: true},
-    noise_level: {type:Number, required: true},
-    cable_length: {type:Number, required: true},
-    company_warranty: {type:Number, required: true},
-    warranty: {type:Number, required: true},
-    origin_country: {type: String, required: true}
-})
-
-const vacuumDetails = new Schema ({
-    productType: {type:String, required: true},
-    trademark: {type:String, required: true},
-    container_size: {type:Number, required: true},
-    height: {type: Number, required: true},
-    weight: {type: Number, required: true},
-    power: {type: Number, required: true},
-    dry_vacuum: {type: Boolean, required: true},
-    liquid_vacuum: {type: Boolean, required: true},
-    vacuum_type: {type: String, required: true},
-    cable_length: {type: String, required: true},
-    warranty: {type: String, required: true},
-    company_warranty: {type:String, required: true},
-    origin_country: {type:String, required: true}
-})
-    */
